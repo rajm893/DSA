32216a9e91b74dd1d7b1e741fba28ab97806a291
Raj Mudigonda <rajmudigonda893@gmail.com>
2024-01-12 17:15:08 +0000

    Time: 53 ms (71.29%) | Memory: 17.4 MB (21.92%) - LeetSync

---
+++ Leetcode_easy/657-robot-return-to-origin/robot-return-to-origin.py
@@ -0,0 +1,13 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        
+        steps = {'U':[0,1],
+                'D': [0,-1],
+                'L': [1,-1],
+                'R': [1,1]}
+
+        origin = [0,0]
+        for move in moves:
+            dir, diff = steps[move]
+            origin[dir] += diff
+        return origin[0] == origin[1] == 0
