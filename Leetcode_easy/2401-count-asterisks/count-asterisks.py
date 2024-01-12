3972ec6c3034fa6a538ec9a7caecc842f651c3e3
Raj Mudigonda <rajmudigonda893@gmail.com>
2024-01-12 17:23:46 +0000

    Time: 40 ms (54.63%) | Memory: 17.1 MB (33.21%) - LeetSync

---
+++ Leetcode_easy/2401-count-asterisks/count-asterisks.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countAsterisks(self, s: str) -> int:
+        count = 0
+        ans = 0
+        for i in s:
+            if i == '|':
+                count += 1
+            if count % 2 == 0:
+                if i == '*':
+                    ans += 1
+        return ans
