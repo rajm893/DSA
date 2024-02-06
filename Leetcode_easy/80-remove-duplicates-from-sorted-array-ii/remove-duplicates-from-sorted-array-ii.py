a45e6d9d460b058cd1dbb3b396a1389f67e1e7bf
Raj Mudigonda <rajmudigonda893@gmail.com>
2024-02-06 16:09:57 +0000

    Time: 52 ms (68.25%) | Memory: 16.6 MB (74.86%) - LeetSync

---
+++ Leetcode_easy/80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -0,0 +1,10 @@
+# Check once more
+# This way we overwrite any "triple duplicates" in the array.
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        k = 2
+        for i in range(2,len(nums)):
+            if  nums[i] != nums[k-2]:
+                nums[k] = nums[i]
+                k += 1
+        return k
