2ffa0d3d4a7d2e9a700a1860261f97ebe9dca93a
Raj Mudigonda <rajmudigonda893@gmail.com>
2024-02-13 22:04:44 +0000

    Time: 939 ms (78.32%) | Memory: 0B (100.00%) - LeetSync

--- Leetcode_easy/1317-monthly-transactions-i/monthly-transactions-i.sql
+++ Leetcode_easy/1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -5,4 +5,4 @@ sum(case when state='approved' then 1 else 0 end) as approved_count,
 sum(amount) as trans_total_amount,
 sum(case when state='approved' then amount else 0 end) as approved_total_amount
 from Transactions 
-group by left(trans_date,7), country
+group by month, country
