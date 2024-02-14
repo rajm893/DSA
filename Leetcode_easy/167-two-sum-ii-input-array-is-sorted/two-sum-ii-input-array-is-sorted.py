9f2e7a7dcda981d7f5f506fe5ade4167f4aca844
Raj Mudigonda <rajmudigonda893@gmail.com>
2024-02-14 15:33:20 +0000

    Time: 101 ms (85.57%) | Memory: 17.6 MB (62.26%) - LeetSync

--- Leetcode_easy/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ Leetcode_easy/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,12 +1,15 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        
-        seen = {}
-        for i in range(len(numbers)):
-            second_num = target - numbers[i]
-            if second_num in seen:
-                return [seen[second_num]+1, i+1]
-            seen[numbers[i]] = i
+        # we have to use 2 pointers to solve it linear time
 
+        l = 0
+        r = len(numbers)-1
 
-                
+        while l < r:
+            curr = numbers[l] + numbers[r]
+            if curr > target:
+                r -= 1
+            elif curr < target:
+                l += 1
+            else:
+                return [l+1, r+1]
