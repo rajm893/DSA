84173e896480c0d235a289b05dba8e1c700926ab
Raj Mudigonda <rajmudigonda893@gmail.com>
2024-02-15 17:25:33 +0000

    Time: 37 ms (64.36%) | Memory: 16.5 MB (86.19%) - LeetSync

---
+++ Leetcode_easy/202-happy-number/happy-number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        
+
+        visited = set()
+        
+        def sumOfSquares(n):
+            res = 0    
+            while n:
+                num = n%10
+                res += num**2
+                n = n // 10
+            return res
+
+        while n not in visited:
+            visited.add(n)
+            n = sumOfSquares(n)
+            if n == 1:
+                return True
+        return False
