b1d384988daa32a4dcf2fdf0adf495c0d340e95e
Raj Mudigonda <rajmudigonda893@gmail.com>
2024-07-31 22:45:04 +0100

    Time: 171 ms (54.93%) | Memory: 18 MB (15.87%) - LeetSync

--- Leetcode_easy/169-majority-element/majority-element.py
+++ Leetcode_easy/169-majority-element/majority-element.py
@@ -2,8 +2,7 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         
         maj = len(nums) // 2
-        nums_set = set(nums)
-        for i in nums_set:
+        unique_nums = set(nums)
+        for i in unique_nums:
             if nums.count(i) > maj:
                 return i
-        
